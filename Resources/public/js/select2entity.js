23af700f2a067b4539c107bc52b6bd6c67b60e30
Martijn Hartlief <martijn@hcdesigns.nl>
2016-08-09 12:09:45 +0200

    Don't create new tag when input length is 0

--- Resources/public/js/select2entity.js
+++ Resources/public/js/select2entity.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             $s2.select2($.extend({
                 // Tags support
                 createTag: function (data) {
-                    if ($s2.data('tags')) {
+                    if ($s2.data('tags') && data.term.length > 0) {
                         var text = data.term + $s2.data('tags-text');
                         return { id: $s2.data('new-tag-prefix')+data.term, text: text }; 
                     }
